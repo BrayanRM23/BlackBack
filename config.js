4a5d9fda834e23b5d5ca2b95379dfc12bb9e9bae
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-11-28 22:04:36 -0500

    PAPA

---
+++ config.js
@@ -0,0 +1,8 @@
+import {config} from "dotenv"
+
+config()
+
+export const BUCKET_NAME_PAPA = process.env.BUCKET_NAME_PAPA
+export const REGION_PAPA = process.env.REGION_PAPA
+export const PUBLIC_PAPA = process.env.PUBLIC_PAPA
+export const UNA_PAPA = process.env.UNA_PAPA
---
+++ s3.js
@@ -0,0 +1,67 @@
+import {S3Client, PutObjectCommand, ListObjectsCommand, GetObjectCommand} from "@aws-sdk/client-s3"
+import {BUCKET_NAME_PAPA, REGION_PAPA, PUBLIC_PAPA, UNA_PAPA} from './config.js'
+import fs from 'fs'
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+
+const client = new S3Client ({
+    region: REGION_PAPA,
+    credentials: {
+        accessKeyId: PUBLIC_PAPA,
+        secretAccessKey: UNA_PAPA
+    }
+})
+
+
+export async function uploadFile(file) {
+  const stream = fs.createReadStream(file.tempFilePath);
+  const uploadParams = {
+    Bucket: process.env.BUCKET_NAME_PAPA,
+    Key: file.name,
+    Body: stream,
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  await client.send(command);
+
+  // Generar la URL del archivo en S3
+  const fileURL = `https://${process.env.BUCKET_NAME_PAPA}.s3.amazonaws.com/${file.name}`;
+  return fileURL;
+}
+
+
+export async function getFiles() {
+    const command= new ListObjectsCommand({
+        Bucket: BUCKET_NAME_PAPA
+    })
+    return await client.send(command)
+    
+}
+
+export async function getfile(filename) {
+    const command = new GetObjectCommand({
+        Bucket: BUCKET_NAME_PAPA,
+        Key: filename
+    })
+    return await client.send(command)
+    
+}
+
+export async function downloadfile(filename) {
+    const command = new GetObjectCommand({
+        Bucket: BUCKET_NAME_PAPA,
+        Key: filename
+    })
+    const result = await client.send(command)
+    console.log(result)
+    result.Body.pipe(fs.createWriteStream(`./images/${filename}`))
+    
+}
+
+export async function getfileURL(filename) {
+    const command = new GetObjectCommand({
+        Bucket: BUCKET_NAME_PAPA,
+        Key: filename
+    })
+    return await getSignedUrl(client, command, {expiresIn:3600})
+    
+}
