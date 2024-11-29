0ad5b0eee6b767f601557d76034a7fb5d226c077
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-11-28 22:35:23 -0500

    PAPA4

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-import fileUpload  from 'express-fileupload'
-import cors from 'cors'
-import router from './routes/routes.js'
-import mongoose from 'mongoose'
-import express, { urlencoded, json } from 'express'
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+import router from './routes/routes.js';
+import mongoose from 'mongoose';
+import express, { urlencoded, json } from 'express';
 
-const app = express()
-app.use(cors())
-app.use(urlencoded({ extended: true }))
-app.use(json())
+const app = express();
+app.use(cors());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.use(fileUpload({
     useTempFiles: true,
-    tempFileDir: './uploads'
-}))
+    tempFileDir: './uploads',
+}));
 
-app.use('/papa', router)
+app.use('/papa', router);
 
-app.use(express.static('images'))
+app.use(express.static('images'));
+
+// Ruta para mostrar el mensaje en la raíz
+app.get('/', (req, res) => {
+    res.send('Este es el Backend de Black Tube');
+});
 
 mongoose
-    .connect(process.env.MONGO_URI)  
-    .then(() => console.log("Connected to MongoDB Atlas"))  
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB Atlas"))
     .catch((error) => console.error(error));
-app.listen(4000)
 
-console.log(`server on port ${4000}`)
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
+});
