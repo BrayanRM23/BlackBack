f9e53f125f46af7641fce7772411919c2708792f
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-11-28 22:54:43 -0500

    PAPA7

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ import mongoose from 'mongoose';
 import express, { urlencoded, json } from 'express';
 
 const app = express();
+
+// Middleware de configuración
 app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
+// Configuración de fileUpload con límite de tamaño
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './uploads',
+    limits: { fileSize: 50 * 1024 * 1024 }, // Límite de 50 MB
 }));
 
+// Rutas del backend
 app.use('/papa', router);
 
+// Servir archivos estáticos desde la carpeta 'images'
 app.use(express.static('images'));
 
 // Ruta para mostrar el mensaje en la raíz
@@ -23,12 +29,15 @@ app.get('/', (req, res) => {
     res.send('Este es el Backend de Black Tube');
 });
 
+// Conexión a MongoDB
 mongoose
     .connect(process.env.MONGO_URIEL)
     .then(() => console.log("Connected to MongoDB Atlas"))
     .catch((error) => console.error(error));
 
+// Inicia el servidor
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 });
+
